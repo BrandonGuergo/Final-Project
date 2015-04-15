406720beaac106d9946b35930568a0e19fdc06d2
coreywolfe <cnwolfe@live.com>
2015-04-15 18:33:01 -0400

    Create blackjack.h

---
+++ blackjack.h
@@ -0,0 +1,14 @@
+#ifndef BLACKJACK_H
+#define BLACKJACK_H
+#include <iostream>
+
+class blackjack
+{
+    public:
+        blackjack();
+        void run();
+    private:
+        void play();
+};
+
+#endif // BLACKJACK_H
